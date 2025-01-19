36ad154d83c23658999fe96336ce2ba72be3d6c5
Alejandro Carvajal <ale.carva16@hotmail.com>
2025-01-19 11:55:05 -0600

    solo añade url

--- URLmodifier/firefox/background.js
+++ URLmodifier/firefox/background.js
@@ -5,18 +5,3 @@ document.addEventListener('keydown', function(event) {
         window.location.href = newUrl;
     }
 });
-
-
-// Polyfill para compatibilidad entre navegadores
-if (typeof browser === "undefined") {
-    var browser = chrome;
-}
-
-// Listener para atajo de teclado
-document.addEventListener('keydown', (event) => {
-    if (event.ctrlKey && event.shiftKey) {
-        browser.tabs.executeScript({
-            file: "highlightNet.js"
-        });
-    }
-});
